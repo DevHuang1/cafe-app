8d200f8183f2565d3b72e9bd028542a120a20941
DevHuang1 <ryanthuta77@gmail.com>
2026-05-06 15:35:35 +0630

    added login and signup pages and connected server functions to authenticate and log the user into the system

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -2,9 +2,10 @@
 
 import React, { useState } from "react";
 import Link from "next/link";
-import { signup } from "@/lib/signup/actions";
+
 import { createClient } from "@/utils/supabase/client";
 import OtpModal from "@/components/auth/OtpModal";
+import Signup from "@/lib/signup/actions";
 export default function SignUpPage() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [userEmail, setUserEmail] = useState("");
@@ -14,7 +15,7 @@ export default function SignUpPage() {
     const formData = new FormData(e.currentTarget);
     setUserEmail(formData.get("email"));
 
-    await signup(formData);
+    await Signup(formData);
     setIsModalOpen(true);
   };
   const handleVerifyOtp = async (otp) => {
