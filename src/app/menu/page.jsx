4dcbae30506f0feddae1661948c2ca5c055b7f6f
DevHuang1 <ryanthuta77@gmail.com>
2026-05-08 00:33:29 +0630

    added featured menu

--- src/app/menu/page.jsx
+++ src/app/menu/page.jsx
@@ -1,18 +1,16 @@
-
-'use client';
+"use client";
+import FeaturedMenu from "@/components/Cart/FeaturedMenu";
 import CategoryFilter from "@/components/Menu/CategoryFilter";
 import SearchBar from "@/components/Menu/SearchBar";
 
 export default function MenuPage() {
   return (
     <div className="min-h-screen bg-bg-main">
-      
       {/* Search bar full width at top */}
       <SearchBar />
 
       {/* Sidebar + Content side by side */}
       <div className="flex">
-        
         {/* Category sidebar on the left */}
         <div className="w-48 shrink-0">
           <CategoryFilter />
@@ -21,9 +19,9 @@ export default function MenuPage() {
         {/* Main content on the right */}
         <main className="flex-1 p-8">
           {/* menu items go here */}
+          <FeaturedMenu />
         </main>
-
       </div>
     </div>
   );
-} 
+}
