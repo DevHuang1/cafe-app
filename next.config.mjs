a7ca04e1154922b9828b64f06994803b2714dde0
Kimjurubyjane-031 <jjlrforrealblink@gmail.com>
2026-06-02 00:07:19 +0630

    fix layout and update features

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['172.16.0.2'],
 };
 
 export default nextConfig;
