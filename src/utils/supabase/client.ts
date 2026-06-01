7e6390227059da17b10262f1ba23a0495b85e1d1
DevHuang1 <ryanthuta77@gmail.com>
2026-06-02 00:26:14 +0630

    updates

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,10 +1,7 @@
 import { createBrowserClient } from "@supabase/ssr";
 
-console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("KEY starts with:", process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY?.slice(0, 20));
-
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
   );
